93ac79b95960e8db69d18fa52d8ed38072393375
Stein Magnus Jodal <stein.magnus@jodal.no>
2014-06-28 00:14:24 +0200

    Install GStreamer with a good selection of codecs
    The current selection is taken from the Rompr wiki:
    http://sourceforge.net/p/rompr/wiki/Installation%20on%20Mac%20OS%20X/

--- mopidy.rb
+++ mopidy.rb
@@ -7,9 +7,35 @@ class Mopidy < Formula
   head 'https://github.com/mopidy/mopidy.git'
 
   depends_on :python
+  depends_on 'homebrew/versions/gst-plugins-base010' => [
+    'with-libogg',
+    'with-libvorbis',
+    'with-theora',
+  ]
+  depends_on 'homebrew/versions/gst-plugins-good010' => [
+    'with-flac',
+    'with-jpeg',
+    'with-libcaca',
+    'with-libshout',
+    'with-libsoup',
+    'with-speex',
+    'with-taglib',
+  ]
+  depends_on 'homebrew/versions/gst-plugins-bad010' => [
+    'with-faad2',
+    'with-libmms',
+    'with-libsndfile',
+  ]
+  depends_on 'homebrew/versions/gst-plugins-ugly010' => [
+    'with-flac',
+    'with-lame',
+    'with-libmms',
+    'with-libvorbis',
+    'with-mad',
+    'with-theora',
+    'with-two-lame',
+  ]
   depends_on 'homebrew/versions/gst-python010'
-  depends_on 'homebrew/versions/gst-plugins-good010'
-  depends_on 'homebrew/versions/gst-plugins-ugly010'
   depends_on 'mopidy/mopidy/pykka'
 
   def install
