e42bcccdc3f4770646883ad8eff247397d7c2e3f
Stein Magnus Jodal <stein.magnus@jodal.no>
2014-07-24 09:39:06 +0200

    Add formula for backports.ssl_match_hostname

---
+++ backports.ssl-match-hostname.rb
@@ -0,0 +1,24 @@
+require 'formula'
+
+class BackportsSslMatchHostname < Formula
+  homepage 'https://pypi.python.org/pypi/backports.ssl_match_hostname'
+  url 'https://pypi.python.org/packages/source/b/backports.ssl_match_hostname/backports.ssl_match_hostname-3.4.0.2.tar.gz#md5=788214f20214c64631f0859dc79f23c6'
+  sha1 'da4e41f3b110279d2382df47ac1e4f10c63cf954'
+  head 'https://bitbucket.org/brandon/backports.ssl_match_hostname'
+
+  depends_on :python => :recommended
+  depends_on :python3 => :optional
+
+  def install
+    Language::Python.each_python(build) do |python, version|
+      system python, "setup.py", "install", "--prefix=#{prefix}",
+        "--single-version-externally-managed", "--record=installed.txt"
+    end
+  end
+
+  def test
+    Language::Python.each_python(build) do |python, version|
+      system python, "-c", "import backports.ssl_match_hostname"
+    end
+  end
+end
