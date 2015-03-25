110ce8b42fac52737a1c660c31cefbaa2f00de27
Stein Magnus Jodal <stein.magnus@jodal.no>
2015-03-25 23:28:27 +0100

    Requests 2.6.0

--- requests.rb
+++ requests.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Requests < Formula
   homepage 'http://python-requests.org/'
-  url 'https://pypi.python.org/packages/source/r/requests/requests-2.3.0.tar.gz'
-  sha1 'f57bc125d35ec01a81afe89f97dc75913a927e65'
+  url 'https://pypi.python.org/packages/source/r/requests/requests-2.6.0.tar.gz'
+  sha1 'ad7327c73e8be8c188ad489d511097202b1fef12'
   head 'https://github.com/kennethreitz/requests.git'
 
   depends_on :python => :recommended
