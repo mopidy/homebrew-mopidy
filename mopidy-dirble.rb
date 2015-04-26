778582c268c22661466306f237928698ff411ac7
Stein Magnus Jodal <stein.magnus@jodal.no>
2015-04-26 22:58:44 +0200

    Mopidy-Dirble 1.1.0

--- mopidy-dirble.rb
+++ mopidy-dirble.rb
@@ -2,14 +2,15 @@ require 'formula'
 
 class MopidyDirble < Formula
   homepage 'https://github.com/mopidy/mopidy-dirble'
-  url 'https://pypi.python.org/packages/source/M/Mopidy-Dirble/Mopidy-Dirble-1.0.0.tar.gz'
-  sha1 '51d42bc76e80f0c2c3aeeb2b98380e0e23bf82c7'
+  url 'https://pypi.python.org/packages/source/M/Mopidy-Dirble/Mopidy-Dirble-1.1.0_1.tar.gz'
+  sha1 '22f5739fa00c2f752b3bc42595dd4e0ee1afe512'
   head 'https://github.com/mopidy/mopidy-dirble.git'
 
   depends_on :python
   depends_on 'mopidy/mopidy/mopidy'
   depends_on 'mopidy/mopidy/pykka'
   depends_on 'mopidy/mopidy/pycountry'
+  depends_on 'mopidy/mopidy/requests'
 
   def install
     system "python", "setup.py", "install", "--prefix=#{prefix}",
