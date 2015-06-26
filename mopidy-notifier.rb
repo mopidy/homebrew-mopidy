3a44c14a527db78b8bfc1ac1f60aab5582442b0b
Stein Magnus Jodal <stein.magnus@jodal.no>
2015-06-26 11:47:33 +0200

    Mopidy-Notifier 0.3.3

--- mopidy-notifier.rb
+++ mopidy-notifier.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class MopidyNotifier < Formula
   homepage 'https://github.com/sauberfred/mopidy-notifier'
-  url 'https://pypi.python.org/packages/source/M/Mopidy-Notifier/Mopidy-Notifier-0.3.2.tar.gz'
-  sha1 'f3afe6cb2a215d1a0cd645c1e4ae85543246a0e5'
+  url 'https://pypi.python.org/packages/source/M/Mopidy-Notifier/Mopidy-Notifier-0.3.3.tar.gz'
+  sha1 'd5eecbc99d19b004e912a538d70f5cfad74cb417'
   head 'https://github.com/sauberfred/mopidy-notifier.git'
 
   depends_on :python
