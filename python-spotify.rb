6728c26d409c39dce97147d81dee69ffa482794c
Stein Magnus Jodal <stein.magnus@jodal.no>
2015-09-05 00:21:59 +0200

    python-spotify 2.0.3

--- python-spotify.rb
+++ python-spotify.rb
@@ -1,8 +1,8 @@
 class PythonSpotify < Formula
   desc "Python binding for libspotify"
   homepage "http://pyspotify.mopidy.com/"
-  url "https://pypi.python.org/packages/source/p/pyspotify/pyspotify-2.0.2.tar.gz"
-  sha256 "46211fcf7746de027515ecf2c41cd779c5fe7f9103fce52cff33f3726c3bc8b5"
+  url "https://pypi.python.org/packages/source/p/pyspotify/pyspotify-2.0.3.tar.gz"
+  sha256 "899bb134b9cfb34efe443194ce477c338525efd575aa4b8033393b9803fb35cb"
   head "https://github.com/mopidy/pyspotify.git", :branch => "v2.x/develop"
 
   depends_on :python
