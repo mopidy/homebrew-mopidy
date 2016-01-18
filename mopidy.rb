aa05065347a73d13778c18549120016fdb935d03
Stein Magnus Jodal <stein.magnus@jodal.no>
2016-01-18 23:10:43 +0100

    Mopidy 1.1.2

--- mopidy.rb
+++ mopidy.rb
@@ -1,8 +1,8 @@
 class Mopidy < Formula
   desc "Extensible music server written in Python"
   homepage "http://www.mopidy.com/"
-  url "https://pypi.python.org/packages/source/M/Mopidy/Mopidy-1.1.1.tar.gz"
-  sha256 "823e1ba8781a107c69e8c1cd8fad9be81dc003c946e9d54fe41cadaad3a0e78e"
+  url "https://pypi.python.org/packages/source/M/Mopidy/Mopidy-1.1.2.tar.gz"
+  sha256 "474089ee563ff1fe96e5bcad37a731dd3207cbf0fdea952d5b805f464a2b0bc8"
   head "https://github.com/mopidy/mopidy.git"
 
   depends_on :python
