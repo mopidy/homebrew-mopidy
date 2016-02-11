af4e9d4a6ac4e23631dc546f49a26fe109a44f47
Stein Magnus Jodal <stein.magnus@jodal.no>
2016-02-11 14:10:44 +0100

    python-backports-ssl-match-hostname 3.5.0.1

--- python-backports-ssl-match-hostname.rb
+++ python-backports-ssl-match-hostname.rb
@@ -1,8 +1,8 @@
 class PythonBackportsSslMatchHostname < Formula
   desc "Backport of ssl.match_hostname() function"
   homepage "https://pypi.python.org/pypi/backports.ssl_match_hostname"
-  url "https://pypi.python.org/packages/source/b/backports.ssl_match_hostname/backports.ssl_match_hostname-3.4.0.2.tar.gz"
-  sha256 "07410e7fb09aab7bdaf5e618de66c3dac84e2e3d628352814dc4c37de321d6ae"
+  url "https://pypi.python.org/packages/source/b/backports.ssl_match_hostname/backports.ssl_match_hostname-3.5.0.1.tar.gz"
+  sha256 "502ad98707319f4a51fa2ca1c677bd659008d27ded9f6380c79e8932e38dcdf2"
   head "https://bitbucket.org/brandon/backports.ssl_match_hostname"
 
   depends_on :python => :recommended
