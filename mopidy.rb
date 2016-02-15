7a876e87cd9e9c20de5bb06c066ae7222a8066a4
Stein Magnus Jodal <stein.magnus@jodal.no>
2016-02-15 22:29:59 +0100

    Mopidy 2.0.0

--- mopidy.rb
+++ mopidy.rb
@@ -1,17 +1,17 @@
 class Mopidy < Formula
   desc "Extensible music server written in Python"
   homepage "http://www.mopidy.com/"
-  url "https://pypi.python.org/packages/source/M/Mopidy/Mopidy-1.1.2.tar.gz"
-  sha256 "474089ee563ff1fe96e5bcad37a731dd3207cbf0fdea952d5b805f464a2b0bc8"
+  url "https://pypi.python.org/packages/source/M/Mopidy/Mopidy-2.0.0.tar.gz"
+  sha256 "14a04c249f83d42f2012b580f3a05853f56320f1bb68ac91c4068b64c81a9265"
   head "https://github.com/mopidy/mopidy.git"
 
   depends_on :python
-  depends_on "homebrew/versions/gst-plugins-base010" => [
+  depends_on "gst-plugins-base" => [
     "with-libogg",
     "with-libvorbis",
     "with-theora"
   ]
-  depends_on "homebrew/versions/gst-plugins-good010" => [
+  depends_on "gst-plugins-good" => [
     "with-flac",
     "with-jpeg",
     "with-libshout",
@@ -19,21 +19,22 @@ class Mopidy < Formula
     "with-speex",
     "with-taglib"
   ]
-  depends_on "homebrew/versions/gst-plugins-bad010" => [
+  depends_on "gst-plugins-bad" => [
     "with-faad2",
     "with-libmms",
-    "with-libsndfile"
+    "with-opus"
   ]
-  depends_on "homebrew/versions/gst-plugins-ugly010" => [
+  depends_on "gst-plugins-ugly" => [
     "with-flac",
     "with-lame",
     "with-libmms",
     "with-libvorbis",
     "with-mad",
+    "with-libshout",
     "with-theora",
     "with-two-lame"
   ]
-  depends_on "homebrew/versions/gst-python010"
+  depends_on "gst-python"
   depends_on "mopidy/mopidy/python-pykka"
   depends_on "mopidy/mopidy/python-tornado"
   depends_on "mopidy/mopidy/python-requests"
