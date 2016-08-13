38b226182cb0ec0da82c5dc4c7dece5beafb01fc
Stein Magnus Jodal <stein.magnus@jodal.no>
2016-08-13 17:17:47 +0200

    python-requests 2.11.0

--- python-requests.rb
+++ python-requests.rb
@@ -1,8 +1,8 @@
 class PythonRequests < Formula
   desc "Python library for making HTTP requests"
   homepage "http://python-requests.org/"
-  url "https://pypi.python.org/packages/source/r/requests/requests-2.9.1.tar.gz"
-  sha256 "c577815dd00f1394203fc44eb979724b098f88264a9ef898ee45b8e5e9cf587f"
+  url "https://pypi.python.org/packages/8d/66/649f861f980c0a168dd4cccc4dd0ed8fa5bd6c1bed3bea9a286434632771/requests-2.11.0.tar.gz\#md5\=147afc07d8967cf46359701551a6cb69"
+  sha256 "b2ff053e93ef11ea08b0e596a1618487c4e4c5f1006d7a1706e3671c57dea385"
   head "https://github.com/kennethreitz/requests.git"
 
   depends_on :python => :recommended
