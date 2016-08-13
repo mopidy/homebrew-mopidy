5176c2e522f3884d4ced118a3e13b4fb6b0a6205
Stein Magnus Jodal <stein.magnus@jodal.no>
2016-08-13 17:19:52 +0200

    python-tornado 4.4.1

--- python-tornado.rb
+++ python-tornado.rb
@@ -1,8 +1,8 @@
 class PythonTornado < Formula
   desc "Python web framework and asynchronous networking library"
   homepage "http://www.tornadoweb.org/"
-  url "https://pypi.python.org/packages/source/t/tornado/tornado-4.3.tar.gz"
-  sha256 "c9c2d32593d16eedf2cec1b6a41893626a2649b40b21ca9c4cac4243bde2efbf"
+  url "https://pypi.python.org/packages/96/5d/ff472313e8f337d5acda5d56e6ea79a43583cc8771b34c85a1f458e197c3/tornado-4.4.1.tar.gz"
+  sha256 "371d0cf3d56c47accc66116a77ad558d76eebaa8458a6b677af71ca606522146"
   head "https://github.com/tornadoweb/tornado.git"
 
   depends_on :python => :recommended
