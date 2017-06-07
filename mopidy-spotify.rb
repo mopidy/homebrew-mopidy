d75f4bc42d21f50200f8e7287a73ea90c27ef6b9
Stein Magnus Jodal <stein.magnus@jodal.no>
2017-06-08 00:33:06 +0200

    mopidy-spotify 3.1.0

--- mopidy-spotify.rb
+++ mopidy-spotify.rb
@@ -1,8 +1,8 @@
 class MopidySpotify < Formula
   desc "Mopidy extension for playing music from Spotify"
   homepage "https://github.com/mopidy/mopidy-spotify"
-  url "https://pypi.python.org/packages/source/M/Mopidy-Spotify/Mopidy-Spotify-3.0.0.tar.gz"
-  sha256 "441caefa0713288a10e9d6de28dec12a9c7ab2c70089cfe4f824825f37fba45f"
+  url "https://pypi.python.org/packages/6b/e8/1e16bada383089cf980630b8fdf2b2c44ae7d1a6afd8e1a7a49947465f38/Mopidy-Spotify-3.1.0.tar.gz"
+  sha256 "f5123a4ce310eca7d16fcb693839729249df86610ed779e7cea17927b3ac8b66"
   head "https://github.com/mopidy/mopidy-spotify.git"
 
   depends_on :python
