a51a196c0e548eebe859cd749c964917ae239673
Stein Magnus Jodal <stein.magnus@jodal.no>
2018-06-04 01:18:49 +0200

    Mopidy-Scrobbler 1.2.0 with bundled dependencies

--- Formula/mopidy-scrobbler.rb
+++ Formula/mopidy-scrobbler.rb
@@ -1,19 +1,32 @@
 class MopidyScrobbler < Formula
   desc "Mopidy extension for scrobbling played tracks to Last.fm"
   homepage "https://github.com/mopidy/mopidy-scrobbler"
-  url "https://files.pythonhosted.org/packages/source/M/Mopidy-Scrobbler/Mopidy-Scrobbler-1.1.1.tar.gz"
-  sha256 "7951dcf4db088fe755145a16a853546fbf3cf7a408d2120d50580ed3db0a85c3"
+  url "https://files.pythonhosted.org/packages/23/f0/f9c4766e14d8ff4196df78118fe91743a467a50c3b5ac072a4c9a5cf24ad/Mopidy-Scrobbler-1.2.0.tar.gz"
+  sha256 "47ee27fab12cd12d8e837bc97aaf46cfae00eaccc37c8b44ac4254b0f0615909"
   head "https://github.com/mopidy/mopidy-scrobbler.git"
 
   depends_on "python@2" if MacOS.version <= :snow_leopard
   depends_on "mopidy/mopidy/mopidy"
-  depends_on "mopidy/mopidy/python-pylast"
 
   # Dependencies assumed bundled by mopidy:
   # - pykka
 
+  resource "pylast" do
+    # Required by tornado
+    url "https://files.pythonhosted.org/packages/8a/1a/ece4ef4ebf51236ac25e9708fb3e1e70b6447e01262f8b156ccbda894fa9/pylast-2.2.0.tar.gz"
+    sha256 "a21a10e559cbb80db5eb72e20a22740496a292977ed3568c937560b8d6885ab4"
+  end
+
   def install
-    system "python", *Language::Python.setup_install_args(prefix)
+    resource("pylast").stage do
+      system "python", *Language::Python.setup_install_args(libexec)
+    end
+
+    system "python", *Language::Python.setup_install_args(libexec)
+
+    site_packages = "lib/python2.7/site-packages"
+    pth_contents = "import site; site.addsitedir('#{libexec/site_packages}')\n"
+    (prefix/site_packages/"homebrew-mopidy-scrobbler.pth").write pth_contents
   end
 
   test do
