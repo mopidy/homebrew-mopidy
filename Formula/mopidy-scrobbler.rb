eac07bd3d8d420efc7e1496bfe51c834ca8c34c1
Stein Magnus Jodal <stein.magnus@jodal.no>
2019-12-17 22:56:39 +0100

    Mopidy-Scrobbler 2.0.0rc1

--- Formula/mopidy-scrobbler.rb
+++ Formula/mopidy-scrobbler.rb
@@ -1,34 +1,37 @@
 class MopidyScrobbler < Formula
   desc "Mopidy extension for scrobbling played tracks to Last.fm"
   homepage "https://github.com/mopidy/mopidy-scrobbler"
-  url "https://files.pythonhosted.org/packages/23/f0/f9c4766e14d8ff4196df78118fe91743a467a50c3b5ac072a4c9a5cf24ad/Mopidy-Scrobbler-1.2.0.tar.gz"
-  sha256 "47ee27fab12cd12d8e837bc97aaf46cfae00eaccc37c8b44ac4254b0f0615909"
+  url "https://files.pythonhosted.org/packages/a8/73/32f00830ca4762bd67c89044c5124d30f38d919cd6214c5d5abbeea4d3ef/Mopidy-Scrobbler-2.0.0rc1.tar.gz"
+  sha256 "2735bf752ae32ae322b51aa5cf4c7b5210cd17d2cd2d89a7dbd6bfb6e2b4e8b1"
   head "https://github.com/mopidy/mopidy-scrobbler.git"
 
-  depends_on "python@2" if MacOS.version <= :snow_leopard
+  depends_on "python"
   depends_on "mopidy/mopidy/mopidy"
 
   # Dependencies assumed bundled by mopidy:
   # - pykka
 
   resource "pylast" do
-    url "https://files.pythonhosted.org/packages/8a/1a/ece4ef4ebf51236ac25e9708fb3e1e70b6447e01262f8b156ccbda894fa9/pylast-2.2.0.tar.gz"
-    sha256 "a21a10e559cbb80db5eb72e20a22740496a292977ed3568c937560b8d6885ab4"
+    url "https://files.pythonhosted.org/packages/c1/3b/05414f6c406d571604a6ee19530ba0a0bd35a8c2cae158ffac0caaa74179/pylast-3.1.0.tar.gz"
+    sha256 "7eb58682beccc0052c3284aba64a9a6454b5e89706f789ebeb2403dc1045606a"
   end
 
   def install
-    resource("pylast").stage do
-      system "python", *Language::Python.setup_install_args(libexec)
+    resources.each do |r|
+      r.stage do
+        system "python3", *Language::Python.setup_install_args(libexec)
+      end
     end
 
-    system "python", *Language::Python.setup_install_args(libexec)
+    system "python3", *Language::Python.setup_install_args(libexec)
 
-    site_packages = "lib/python2.7/site-packages"
+    xy = Language::Python.major_minor_version "python3"
+    site_packages = "lib/python#{xy}/site-packages"
     pth_contents = "import site; site.addsitedir('#{libexec/site_packages}')\n"
     (prefix/site_packages/"homebrew-mopidy-scrobbler.pth").write pth_contents
   end
 
   test do
-    system "python", "-c", "import mopidy_scrobbler"
+    system "python3", "-c", "import mopidy_scrobbler"
   end
 end
