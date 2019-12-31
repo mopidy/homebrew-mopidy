a402f1133f0a5ad969eab50c022c986477ec2b57
Stein Magnus Jodal <stein.magnus@jodal.no>
2019-12-31 11:06:32 +0100

    Mopidy-TuneIn 1.0.0

---
+++ Formula/mopidy-tunein.rb
@@ -0,0 +1,27 @@
+class MopidyTunein < Formula
+  desc "Mopidy extension for playing music from TuneIn"
+  homepage "https://github.com/kingosticks/mopidy-tunein"
+  url "https://files.pythonhosted.org/packages/06/57/bc06f47a2d5e716b15bc3b74d859854386dd5c9ebf7d73a025c32d267945/Mopidy-TuneIn-1.0.0.tar.gz"
+  sha256 "d261730b918db2d81af204ee42c40d33d73fefe9aafe0225d652214e9c56da46"
+  head "https://github.com/kingosticks/mopidy-tunein.git"
+
+  depends_on "python"
+  depends_on "mopidy/mopidy/mopidy"
+
+  # Dependencies assumed bundled by mopidy:
+  # - pykka
+  # - requests
+
+  def install
+    system "python3", *Language::Python.setup_install_args(libexec)
+
+    xy = Language::Python.major_minor_version "python3"
+    site_packages = "lib/python#{xy}/site-packages"
+    pth_contents = "import site; site.addsitedir('#{libexec/site_packages}')\n"
+    (prefix/site_packages/"homebrew-mopidy-tunein.pth").write pth_contents
+  end
+
+  test do
+    system "python3", "-c", "import mopidy_tunein"
+  end
+end
