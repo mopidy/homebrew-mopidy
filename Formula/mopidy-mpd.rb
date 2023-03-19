9bb7bde2d18dbae1c9903fca63d655726a9e055b
Stein Magnus Jodal <stein.magnus@jodal.no>
2023-03-19 18:54:57 +0100

    Mopidy-MPD 3.3.0

--- Formula/mopidy-mpd.rb
+++ Formula/mopidy-mpd.rb
@@ -1,8 +1,8 @@
 class MopidyMpd < Formula
   desc "Mopidy extension for controlling Mopidy from MPD clients"
   homepage "https://github.com/mopidy/mopidy-mpd"
-  url "https://files.pythonhosted.org/packages/32/ee/6f70528199d1ea7d5dff3b2e21fc1ac42e762797f4ba334c77c6f5e9fd18/Mopidy-MPD-3.1.0.tar.gz"
-  sha256 "d34011dad9a053c149a408c25b0ff552406513063bc9cdaab2bde30e71f81228"
+  url "https://files.pythonhosted.org/packages/8a/45/2ae38c8e83c7e7fd49bda4ce2ee3cd7b2454837ab763a5be192ac657bf98/Mopidy-MPD-3.3.0.tar.gz"
+  sha256 "09e2cc46a8fd73006f42b3b1ed71d557c3230e3c0ea2c38d565b0dda8faf4d53"
   head "https://github.com/mopidy/mopidy-mpd.git"
 
   depends_on "python@3.11"
