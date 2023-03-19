9ad3ba84ba4188587d55a1cd95adcd867b03e06b
Stein Magnus Jodal <stein.magnus@jodal.no>
2023-03-19 19:10:14 +0100

    Mopidy-TuneIn 1.1.0_1
    Same release, but using Python 3.11.

--- Formula/mopidy-tunein.rb
+++ Formula/mopidy-tunein.rb
@@ -4,6 +4,7 @@ class MopidyTunein < Formula
   url "https://files.pythonhosted.org/packages/22/c4/7efb36a75643a249528f191d1a9a130d9d62e4907aa35f4f9853e1fe1dd2/Mopidy-TuneIn-1.1.0.tar.gz"
   sha256 "910a96552d239bfaee6ef3635cf9018f7ef733ac311cd0b11f27334dbd56c107"
   head "https://github.com/kingosticks/mopidy-tunein.git"
+  revision 1
 
   depends_on "python@3.11"
   depends_on "mopidy/mopidy/mopidy"
