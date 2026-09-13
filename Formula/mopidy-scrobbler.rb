23eb85bf56fe8de10ab00afeb307059645f4441c
Stein Magnus Jodal <stein.magnus@jodal.no>
2026-09-13 18:55:34 +0000

    mopidy-scrobbler 3.1.1
    Closes #66.
    
    Signed-off-by: Stein Magnus Jodal <stein.magnus@jodal.no>

--- Formula/mopidy-scrobbler.rb
+++ Formula/mopidy-scrobbler.rb
@@ -3,8 +3,8 @@ class MopidyScrobbler < Formula
 
   desc "Last.fm scrobbler for the Mopidy music server"
   homepage "https://github.com/mopidy/mopidy-scrobbler"
-  url "https://files.pythonhosted.org/packages/2b/ed/053538c109893bda96b411dccc3f671d50e328bcfe1118f31283a2fd11b2/mopidy_scrobbler-3.1.0.tar.gz"
-  sha256 "91805a879794646c054cf97237e20b8ca7195695b87b5f95152bb639294df52c"
+  url "https://files.pythonhosted.org/packages/8f/5f/1fff221adf0c7d1b378896ae1ba472efde736ccdae2febec3b4fcac9b0ed/mopidy_scrobbler-3.1.1.tar.gz"
+  sha256 "03af6d203784627e85ced3ad0670e45ff0a4b07a2ba51da0b8d105c1b845b78f"
   license "Apache-2.0"
 
   bottle do
@@ -18,16 +18,6 @@ class MopidyScrobbler < Formula
 
   pypi_packages exclude_packages: "mopidy"
 
-  resource "anyio" do
-    url "https://files.pythonhosted.org/packages/61/cc/a381afa6efea9f496eff839d4a6a1aed3bfafc7b3ab4b0d1b243a12573dd/anyio-4.14.2.tar.gz"
-    sha256 "cfa139f3ed1a23ee8f88a145ddb5ac7605b8bbfd8592baacd7ce3d8bb4313c7f"
-  end
-
-  resource "h11" do
-    url "https://files.pythonhosted.org/packages/01/ee/02a2c011bdab74c6fb3c75474d40b3052059d95df7e73351460c8588d963/h11-0.16.0.tar.gz"
-    sha256 "4e35b956cf45792e4caa5885e69fba00bdbc6ffafbfa020300e549b208ee5ff1"
-  end
-
   resource "httpcore2" do
     url "https://files.pythonhosted.org/packages/be/ad/f4f0e57345f1870f3e8cb624e058d7eca6e5a27d33bcc3311d9b618734cd/httpcore2-2.12.0.tar.gz"
     sha256 "9293522bba0aa7c4c8e9e3f040c16575bd8868e155a77fa30c7a9085a5eae648"
@@ -36,11 +26,6 @@ class MopidyScrobbler < Formula
   resource "httpx2" do
     url "https://files.pythonhosted.org/packages/7f/f8/579a8b51e42e38ee32647df9f08aa25643ae788e275cc625b199829c4671/httpx2-2.12.0.tar.gz"
     sha256 "7631fe9887a8a2275f4a2540e053aa670fcc50742864a9ae7c66e609fdcf12cf"
-  end
-
-  resource "idna" do
-    url "https://files.pythonhosted.org/packages/5f/f7/abb373e5757eaec4b922b92f97ec8d6d7e057cf06778247604fbc4e7c3f3/idna-3.19.tar.gz"
-    sha256 "5e0811a4383b21dc5838069f801c4fb62113b7447663d2530d2bd6e77b49bf15"
   end
 
   resource "pylast" do
